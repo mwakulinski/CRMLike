d91a35c2cc1d16f8bbead02b4cd68aebcc6c07c1
Michał Wakuliński <michal.wakulinski@dreamstormstudios.com>
2022-08-28 15:50:14 +0200

    add createEmployee

---
+++ src/employee/employee.service.ts
@@ -0,0 +1,21 @@
+import { IEmployee, IEmployeeCreate } from "./interfaces";
+
+export class EmployeeService {
+  public counter = 1;
+  private employees: IEmployee[] = [
+    {
+      id: 0,
+      name: "Michał",
+      surname: "Wakulinski",
+      githubAccount: "mwakulinski",
+      employmentType: "B2B",
+    },
+  ];
+
+  async createEmployee(employee: IEmployeeCreate) {
+    const newEmployee = { id: this.counter, ...employee };
+    this.employees.push(newEmployee);
+    this.counter++;
+    return newEmployee.id;
+  }
+}
