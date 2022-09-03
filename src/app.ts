46bd12e5dde1157c4c24b2b43ace176286937eae
Michał Wakuliński <michal.wakulinski@dreamstormstudios.com>
2022-09-03 16:18:20 +0200

    add controllers initialization

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,14 @@ import { IController } from "./interfaces/controller.interface";
 class App {
   private app: Application = express();
   constructor(private controllers: IController[], private port: number) {
+    this.initializeControllers(this.controllers);
     this.app.use(express.json());
+  }
+
+  private initializeControllers(controllers: IController[]) {
+    controllers.forEach((controller) => {
+      this.app.use("/api", controller.router);
+    });
   }
 
   start() {
