1e05059e410c9834da8b0fbefbeeb92cf1ef1dbc
Michał Wakuliński <michal.wakulinski@dreamstormstudios.com>
2022-09-04 16:40:22 +0200

    add create new employee route

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Router, Request, Response } from "express";
 import { IController } from "../interfaces/controller.interface";
 import EmployeeService from "./employee.service";
+import { IEmployeeCreate } from "./interfaces";
 
 class EmployeeController implements IController {
   constructor(
@@ -13,11 +14,24 @@ class EmployeeController implements IController {
 
   private initializeRoutes() {
     this.router.get(this.path, this.getAllEmployees);
+    this.router.post(this.path, this.createNewEmployee);
   }
 
   getAllEmployees = async (req: Request, res: Response, next: NextFunction) => {
     const employees = await this.employeeService.getAllEmployees();
     return res.status(200).send({ employees });
+  };
+
+  createNewEmployee = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const employeeCreateDto: IEmployeeCreate = req.body;
+    const newEmployeeID = await this.employeeService.createEmployee(
+      employeeCreateDto
+    );
+    return res.status(201).json({ id: newEmployeeID });
   };
 }
 
