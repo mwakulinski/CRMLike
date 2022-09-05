7ef75109ba155a1b1e350e7757fb0663fb903eff
Michał Wakuliński <michal.wakulinski@dreamstormstudios.com>
2022-09-06 00:06:49 +0200

    update controller list

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import "dotenv/config";
 import App from "./app";
 import EmployeeController from "./employee/employee.controller";
 import EmployeeService from "./employee/employee.service";
+import FilesUploaderController from "./uploader/uploader.controller";
+import FilesUploaderService from "./uploader/uploader.service";
 
 const port = Number(process.env.PORT);
-const app = new App([new EmployeeController(new EmployeeService())], port);
+const app = new App(
+  [
+    new EmployeeController(new EmployeeService()),
+    new FilesUploaderController(new FilesUploaderService()),
+  ],
+  port
+);
 
 app.start();
