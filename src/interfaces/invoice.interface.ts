e527556be9904b827e7a5daf7dadc04f5677be47
Michał Wakuliński <michal.wakulinski@dreamstormstudios.com>
2022-09-06 23:28:08 +0200

    add IUploadedInvoice

--- src/interfaces/invoice.interface.ts
+++ src/interfaces/invoice.interface.ts
@@ -1,8 +1,14 @@
 export interface IInvoice {
+  id?: number;
   owner: string;
   customer: string;
   amountDue: number;
   dueDate: Date;
   issueDate: Date;
   subject: string;
+}
+
+export interface IUploadedInvoice {
+  fileUrl: string;
+  details: IInvoice;
 }
