44460cfa1f745632d8edac07ccaec432d9c413fe
Michał Wakuliński <michal.wakulinski@dreamstormstudios.com>
2022-09-10 19:48:40 +0200

    add IFileUploaderService interface

--- src/uploader/uploader.service.ts
+++ src/uploader/uploader.service.ts
@@ -1,7 +1,11 @@
 import { IFileToUpload } from "../interfaces/file.interface";
 import path from "path";
 
-class FilesUploaderService {
+export interface IFilesUploaderService {
+  uploadFile: ({ uploadTo, filePath }: IFileToUpload) => Promise<string>;
+}
+
+class FilesUploaderService implements IFilesUploaderService {
   async uploadFile({ uploadTo, filePath }: IFileToUpload) {
     const fileName = path.basename(filePath);
     return `${fileName} has been successfully uploaded to ${uploadTo}`;
