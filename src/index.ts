5fa280ebfb736b7861dc0dbdb74c1311c3842095
Michał Wakuliński <michal.wakulinski@dreamstormstudios.com>
2022-09-10 19:50:59 +0200

    add IService interface and service object

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import "dotenv/config";
 import App from "./app";
 import EmployeeController from "./employee/employee.controller";
-import EmployeeService from "./employee/employee.service";
+import EmployeeService, { IEmployeeService } from "./employee/employee.service";
+import InvoiceService, { IInvoicesService } from "./invoices/invoices.service";
+import SenderService, { ISenderService } from "./sender/sender.service";
 import FilesUploaderController from "./uploader/uploader.controller";
-import FilesUploaderService from "./uploader/uploader.service";
+import FilesUploaderService, {
+  IFilesUploaderService,
+} from "./uploader/uploader.service";
 
 const port = Number(process.env.PORT);
+
+export interface IServices {
+  employeesService: IEmployeeService;
+  filesService: IFilesUploaderService;
+  invoicesService: IInvoicesService;
+  senderService: ISenderService;
+}
+const services: IServices = {
+  employeesService: new EmployeeService(),
+  filesService: new FilesUploaderService(),
+  invoicesService: new InvoiceService(),
+  senderService: new SenderService(),
+};
+
 const app = new App(
-  [
-    new EmployeeController(new EmployeeService()),
-    new FilesUploaderController(new FilesUploaderService()),
-  ],
+  [new EmployeeController(services), new FilesUploaderController(services)],
   port
 );
 
