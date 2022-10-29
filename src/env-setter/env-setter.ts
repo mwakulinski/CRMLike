1b554f5703efc4c4c7e8920dc3e1d7d2c799ba06
Michał Wakuliński <michal.wakulinski1@gmail.com>
2022-10-30 01:08:32 +0200

    update serPort

--- src/env-setter/env-setter.ts
+++ src/env-setter/env-setter.ts
@@ -1,32 +1,27 @@
-import { Validator } from "../validator/validator";
-
-// type validationFunctionType = (
-//   localVariable: envType,
-//   ...parameter: any[]
-// ) => boolean;
+type validationFunctionType = (
+  localVariable: envType,
+  ...parameters: any[]
+) => boolean;
 type envType = string | undefined;
 
 export class EnvSetter {
-  // static setPort(localVariable: envType, validatorArray: validationFunctionType[]) {
-  //     validatorArray.forEach(validateFunction => {
-  //         if (!validateFunction(localVariable, )) {
-  //             throw new Error(`${localVariable} doesn't meet required criteria`)
-  //         }
-  //     })
-  //     return Number(localVariable)
-  // }
-
-  static setPort(localVariable: envType, range: { min: number; max: number }) {
-    if (!Validator.isTransformableToInteger(localVariable)) {
-      throw new Error(`${localVariable} is not an Integer`);
-    }
-
-    if (!Validator.isInGivenRange(localVariable, range)) {
-      throw new Error(
-        `${localVariable} must be in range ${range.min} - ${range.max}`
-      );
-    }
-
-    return Number(localVariable);
+  static setPort(
+    localVariable: envType,
+    functionsToValidate: {
+      functionToValidate: validationFunctionType;
+      args?: any[];
+    }[]
+  ) {
+    let defaultPortValue;
+    functionsToValidate.some(({ functionToValidate, args }) => {
+      if (!functionToValidate(localVariable, args)) {
+        defaultPortValue = 3000;
+        console.log(
+          `${localVariable} doesn't meet required criteria for port\n setting default porty value: ${defaultPortValue}`
+        );
+        return true;
+      }
+    });
+    return defaultPortValue || Number(localVariable);
   }
 }
