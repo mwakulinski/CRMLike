9ed2ea732f32828866978122443291f5fa7961f3
Michał Wakuliński <michal.wakulinski1@gmail.com>
2022-11-03 09:11:05 +0100

    update employ-service test

--- test/employee-service.spec.ts
+++ test/employee-service.spec.ts
@@ -7,7 +7,20 @@ import { beforeEach } from "mocha";
 import InvoiceRepository from "../src/invoices/invoices.repository";
 import { rejects } from "assert";
 import { IRepositories } from "../src/repositories";
+import { EmployeeCreateType } from "../src/employee/interfaces";
 
+const mockUser_1: EmployeeCreateType = {
+  name: "Maria",
+  surname: "Antonina",
+  githubAccount: "MAExp",
+  employmentType: "B2B",
+};
+const mockUser_2: EmployeeCreateType = {
+  name: "Michal",
+  surname: "Wak",
+  githubAccount: "MichWak",
+  employmentType: "B2B",
+};
 describe("EmployeeService", () => {
   let mockEmployeeService: IEmployeeService;
   let mockRepositories: IRepositories;
@@ -23,78 +36,38 @@ describe("EmployeeService", () => {
   describe("createEmployee", () => {
     it("should return created employee with id", async () => {
       expect(
-        await mockEmployeeService.createEmployee({
-          name: "Maria",
-          surname: "Antonina",
-          githubAccount: "MAExp",
-          employmentType: "B2B",
-        })
+        await mockEmployeeService.createEmployee(mockUser_1)
       ).to.deep.equal({
         id: 0,
-        name: "Maria",
-        surname: "Antonina",
-        githubAccount: "MAExp",
-        employmentType: "B2B",
+        ...mockUser_1,
       });
     });
 
     it("should throw error when employee with given githubAccount already exist", async () => {
-      await mockEmployeeService.createEmployee({
-        name: "Maria",
-        surname: "Antonina",
-        githubAccount: "MAExp",
-        employmentType: "B2B",
-      });
+      await mockEmployeeService.createEmployee(mockUser_1);
 
       await rejects(async () => {
-        await mockEmployeeService.createEmployee({
-          name: "Maria",
-          surname: "Antonina",
-          githubAccount: "MAExp",
-          employmentType: "B2B",
-        });
+        await mockEmployeeService.createEmployee(mockUser_1);
       });
     });
   });
 
   describe("getAllEmployees", () => {
-    it("should return empty array when no employees are added", async () => {
-      expect(await mockEmployeeService.getAllEmployees()).to.deep.equal([]);
-    });
-
     it("should return all employees", async () => {
       //Given two employees in a database
-      await mockRepositories.employeeRepository.createEmployee({
-        name: "Maria",
-        surname: "Antonina",
-        githubAccount: "MAExp",
-        employmentType: "B2B",
-      });
-
-      await mockRepositories.employeeRepository.createEmployee({
-        name: "Michal",
-        surname: "Wak",
-        githubAccount: "MichWak",
-        employmentType: "B2B",
-      });
+      await mockRepositories.employeeRepository.createEmployee(mockUser_1);
+      await mockRepositories.employeeRepository.createEmployee(mockUser_2);
       //when
-
       //Then all two employees should be returned
 
       expect(await mockEmployeeService.getAllEmployees()).to.deep.equal([
         {
           id: 0,
-          name: "Maria",
-          surname: "Antonina",
-          githubAccount: "MAExp",
-          employmentType: "B2B",
+          ...mockUser_1,
         },
         {
           id: 1,
-          name: "Michal",
-          surname: "Wak",
-          githubAccount: "MichWak",
-          employmentType: "B2B",
+          ...mockUser_2,
         },
       ]);
     });
@@ -103,20 +76,12 @@ describe("EmployeeService", () => {
   describe("getEmployeeById", () => {
     it("should return unique employee when employee id is passed", async () => {
       //Given employee in a database
-      await mockRepositories.employeeRepository.createEmployee({
-        name: "Maria",
-        surname: "Antonina",
-        githubAccount: "MAExp",
-        employmentType: "B2B",
-      });
+      await mockRepositories.employeeRepository.createEmployee(mockUser_1);
       //When
       //Then
       expect(await mockEmployeeService.getEmployeeById(0)).to.deep.equal({
         id: 0,
-        name: "Maria",
-        surname: "Antonina",
-        githubAccount: "MAExp",
-        employmentType: "B2B",
+        ...mockUser_1,
       });
     });
 
