fb43b99f8d8785f05f9d09d89910812b56c4b9a8
Michał Wakuliński <michal.wakulinski1@gmail.com>
2022-11-22 22:41:03 +0100

    update employeeSchema

--- src/db/mongoose/schemas/employee-schema.ts
+++ src/db/mongoose/schemas/employee-schema.ts
@@ -7,6 +7,6 @@ import {
 export const employeeSchema = new Schema<EmployeeCreateType>({
   name: { type: String, required: true },
   surname: { type: String, required: true },
-  githubAccount: { type: String, required: true },
+  githubAccount: { type: String, required: true, unique: true },
   employmentType: { type: String, enum: EmploymentTypeArr, required: true },
 });
