4f4b46301e74b4a44ab105c529545dd3117688a3
Michał Wakuliński <michal.wakulinski1@gmail.com>
2022-11-22 22:40:42 +0100

    update MongodbConnector

--- src/db/mongodb.connector.ts
+++ src/db/mongodb.connector.ts
@@ -4,7 +4,13 @@ import { IDbConnector } from "./IDbConnector";
 export class MongodbConnector implements IDbConnector {
   constructor(private dbUrls: string) {}
   async connect() {
-    await mongoose.connect(this.dbUrls);
-    // dodać error domenowy
+    console.log("Connecting to Mongo DB");
+    try {
+      await mongoose.connect(this.dbUrls);
+      console.log("Mongo connected");
+    } catch (error) {
+      console.log(error);
+      throw new Error("Connection to Mongo DB failed due to error: " + error);
+    }
   }
 }
