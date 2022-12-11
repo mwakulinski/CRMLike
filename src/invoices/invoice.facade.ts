d258df411548cfb6c66291e923a2defbb908ac88
Michał Wakuliński <michal.wakulinski1@gmail.com>
2022-12-11 16:43:44 +0100

    update uploadNewInvoice

--- src/invoices/invoice.facade.ts
+++ src/invoices/invoice.facade.ts
@@ -1,10 +1,12 @@
-import { InvoiceUploadType } from "../interfaces/invoice.interface";
+import {
+  InvoiceToUploadType,
+  InvoiceUploadedType,
+} from "../interfaces/invoice.interface";
 import { ISenderService } from "../sender/sender.service";
 import { IFilesUploaderService } from "../uploader/uploader.service";
 import { IInvoicesRepository } from "./invoices.repository";
-import { ISaveNewInvoiceInformationResponse } from "./invoices.repository";
 export interface IInvoiceFacade {
-  uploadNewInvoice: (invoiceToUpload: InvoiceUploadType) => Promise<number>;
+  uploadNewInvoice: (invoiceToUpload: InvoiceToUploadType) => Promise<string>;
 }
 
 class InvoiceFacade implements IInvoiceFacade {
@@ -27,28 +29,25 @@ class InvoiceFacade implements IInvoiceFacade {
     this.senderService = senderService;
   }
 
-  async uploadNewInvoice({ details, fileUrl, uploadTo }: InvoiceUploadType) {
-    const [filesUploaderResponse, { invoiceId }]: [
-      string,
-      ISaveNewInvoiceInformationResponse
-    ] = await Promise.all([
-      this.filesUploaderService.upload({
-        filePath: fileUrl,
-        uploadTo: uploadTo,
-      }),
-      this.invoicesRepository.createNewInvoiceInformation({
-        details,
-        fileUrl,
-        uploadTo,
-      }),
-    ]);
+  async uploadNewInvoice({ details, fileUrl, uploadTo }: InvoiceToUploadType) {
+    const [filesUploaderResponse, { id }]: [string, InvoiceUploadedType] =
+      await Promise.all([
+        this.filesUploaderService.upload({
+          filePath: fileUrl,
+          uploadTo: uploadTo,
+        }),
+        this.invoicesRepository.createNewInvoiceInformation({
+          details,
+          fileUrl,
+          uploadTo,
+        }),
+      ]);
 
     await this.senderService.informAboutNewInvoice({
       ...details,
-      id: invoiceId,
     });
 
-    return invoiceId;
+    return id;
   }
 }
 
