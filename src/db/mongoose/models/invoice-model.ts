6364ee6660e075c9aff5272474f318ce8d0e80b0
Michał Wakuliński <michal.wakulinski1@gmail.com>
2022-12-11 16:40:14 +0100

    update invoiceToUploadModel

--- src/db/mongoose/models/invoice-model.ts
+++ src/db/mongoose/models/invoice-model.ts
@@ -1,8 +1,8 @@
 import mongoose, { Model } from "mongoose";
-import { InvoiceUploadType } from "../../../interfaces/invoice.interface";
+import { InvoiceToUploadType } from "../../../interfaces/invoice.interface";
 import { invoiceUploadSchema } from "../schemas/invoice-schema";
 
-export type invoiceUploadModelType = Model<InvoiceUploadType>;
+export type invoiceToUploadModelType = Model<InvoiceToUploadType>;
 
-export const invoiceUploadModel: invoiceUploadModelType =
-  mongoose.model<InvoiceUploadType>("InvoiceUpload", invoiceUploadSchema);
+export const invoiceToUploadModel: invoiceToUploadModelType =
+  mongoose.model<InvoiceToUploadType>("InvoiceUpload", invoiceUploadSchema);
